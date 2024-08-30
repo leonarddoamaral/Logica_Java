d66f1904bd1a71a32528712df8008d47bf7206a3
Leonardo do Amaral Quinquio <117521716+leonarddoamaral@users.noreply.github.com>
2024-08-30 20:35:50 -0300

    VolumeSolidos.java

--- TreinandoLogica/src/VolumeSolidos.java
+++ TreinandoLogica/src/VolumeSolidos.java
@@ -1,12 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Léo
- */
+import javax.swing.JOptionPane;
 public class VolumeSolidos {
-    
-}
+    public static void main(String[] args) {
+        double vol, raio, high, area;
+        int pergunta;
+        
+        pergunta = Integer.parseInt(JOptionPane.showInputDialog("Qual volume voce deseja descobrir: digite 1 para esfera;2 para cilindro; 3 para cubo"));
+        
+        if (pergunta>=3){ 
+            area= Double.parseDouble(JOptionPane.showInputDialog("Digite a area"));
+            vol= area*area*area ;
+            JOptionPane.showMessageDialog(null, "o volume é de"+ vol);
+        }
+        
+        else if (pergunta>=2){
+            raio=Double.parseDouble(JOptionPane.showInputDialog("Digite o raio"));
+            high=Double.parseDouble(JOptionPane.showInputDialog("Digite a altura"));
+            vol= (raio*raio)*high*3.14;
+            JOptionPane.showMessageDialog(null, "o volume é de"+ vol);
+        }
+        
+        else {
+           raio= Double.parseDouble(JOptionPane.showInputDialog("Digite o raio"));
+           vol= ((raio*raio*raio)*12.56)/3;
+           JOptionPane.showMessageDialog(null, "o volume é de"+ vol);
+        }
+        
